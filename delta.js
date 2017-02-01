a7deec187c4b5e5c0e8ca8d5a82a7caac11a849a
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-02-01 20:43:28 +0100

    add spaces

--- delta.js
+++ delta.js
@@ -1,13 +1,13 @@
 //zadanie 8.5
-var a,b,value;
-a=5;
-b=5;
-value=(a*a)+(2*a*b)-(b*b);
+var a, b, value;
+a = 5;
+b = 5;
+value = (a * a) + (2 * a * b) - (b * b);
 console.log(value);
-if (value>0) {
+if (value > 0) {
 	console.log('wynik dodatni');
 }
-else if (value<0) {
+else if (value < 0) {
 	console.log('wynik ujemny')
 }
 else {
